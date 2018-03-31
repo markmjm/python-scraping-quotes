f78fe71f12ddef0ad91c9391e7941ef193e0fc92
markmjm <markmnei@gmail.com>
2018-03-31 10:58:25 -0400

    dev updates

---
+++ locators/quote_locators.py
@@ -0,0 +1,6 @@
+class QuoteLocators:
+    AUTHOR = 'small.author'
+    CONTENT = 'span.text'
+    TAGS = 'div.tags a.tag'
+
+
---
+++ pages/quotes_page.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+
+from locators.quotes_page_locators import QuotesPageLocators
+from parsers.quote import QuoteParser
+
+class QuotesPage:
+    def __init__(self, page):
+        self.soup = BeautifulSoup(page, 'html.parser')
+
+    @property
+    def quotes(self):
+        locator = QuotesPageLocators.QUOTE
+        quote_tags = self.soup.select(locator)
+        for e in quote_tags:
+            print(e)
+        return [QuoteParser(e) for e in quote_tags]
